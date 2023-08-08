3f966485083c931c39c1b3302b8ae3a281ae70f0
Khushbu <singhkhushbu210@gmail.com>
2023-08-08 04:15:48 -0700

    final modification

--- src/main/java/com/csir/vehiclerequisition/repositories/Entry_moduleRepository.java
+++ src/main/java/com/csir/vehiclerequisition/repositories/Entry_moduleRepository.java
@@ -42,12 +42,12 @@ public interface Entry_moduleRepository extends JpaRepository<Entry_module, Long
 	@Query("Select e from Entry_module e where e.status= :status and e.division_name= :division")
 	List<Entry_module> countByDiv(@Param("status") String status, @Param("division") String division);
 	
-	@Query("Select e from Entry_module e where e.status= :status1 or e.status= :status2 or e.status= :status3 and e.division_name= :division_name or e.cdt>= :from_date or e.cdt<= :to_date ORDER BY e.cdt DESC")
+	@Query("Select e from Entry_module e where e.division_name= :division_name and (e.cdt >= :from_date and e.cdt <= :to_date ) and (e.status= :status1 or e.status= :status2 or e.status= :status3 ) ORDER BY e.cdt DESC")
 	List<Entry_module> filterData(@Param("status1") String status1,@Param("status2") String status2,@Param("status3") String status3, @Param("division_name") String division_name, @Param("from_date") Date from_date, @Param("to_date") Date to_date);
 
 	@Query("Select e from Entry_module e where e.userid= :userid and e.cdt>= :from_date or e.cdt<= :to_date ORDER BY e.cdt DESC")
 	List<Entry_module> filterUserData(@Param("userid") Long userid, @Param("from_date") Date from_date, @Param("to_date") Date to_date);
 	
-	@Query("Select e from Entry_module e where e.division_name= :division_name and e.cdt>= :from_date or e.cdt<= :to_date ORDER BY e.cdt DESC")
+	@Query("Select e from Entry_module e where e.division_name= :division_name and (e.cdt>= :from_date and e.cdt<= :to_date) ORDER BY e.cdt DESC")
 	List<Entry_module> filterSanctionData(@Param("division_name") String division_name, @Param("from_date") Date from_date, @Param("to_date") Date to_date);
 }
